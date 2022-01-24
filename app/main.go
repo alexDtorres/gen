0fd9d9770c6567d5ef8701e68ef769da3cb28a49
Alex D. Torres <alex.torres@loves.com>
2022-01-24 15:59:45 -0600

    update comments per added Makefile

--- app/main.go
+++ app/main.go
@@ -1,52 +1,22 @@
 package main
 
-import (
-	"log"
+import "github.com/alexDtorres/gen"
 
-	"github.com/alexDtorres/gen"
-)
-
-// app gen ?name --basic
+// gen app ?name --basic
 func main() {
-	pkg := install()
+	pkg := gen.Install()
 	Apply := pkg.Apply
 	Eval := pkg.Eval
 	pre := pkg.Pre
 	sess := pkg.Sess
+
 	pre.Check()
 
 	name := Eval(sess)
-	sess.Println("?name = ", name)
 	if err := Apply(name, sess); err != nil {
 		sess.Fatal(err)
 	}
-
-	sess.Printf("want = %q \n", "")
-	sess.Println()
 }
 
 // initial = `ls`
 // final   = concatenate(initial, `ls | grep ${name}`)
-
-type deps struct {
-	Sess  *log.Logger
-	Pre   gen.Checker
-	Eval  func(*log.Logger) string
-	Apply func(string, *log.Logger) gen.AppErr
-}
-
-func install() deps {
-	d := deps{}
-	l := log.Default()
-	n := 3
-
-	d.Apply = gen.Apply
-	d.Eval = gen.Eval
-	d.Pre = gen.Need{
-		N:   n,
-		Log: l,
-	}
-	d.Sess = l
-
-	return d
-}
