847e7be263807e3c95e9057a08184ffbae6d347e
Alex D. Torres <alex.torres@loves.com>
2022-01-25 16:43:12 -0600

    finally settle on package structure

--- app/README.md
+++ app/README.md
@@ -1,17 +1,16 @@
-# `gen/drivers`
+# `gen/app`
 
 ## Installation
 
 ### via `go install ...`
 
 `go install github.com/alexDtorres/gen/drivers/app@latest`
-`go install github.com/alexDtorres/gen/drivers/xsd@latest`
 
 
 ## Usage
 
 ### via `/bin/sh`
 
-`app gen <your_app_name>`
-`xsd gen <xml_tmpl_file>`
+`app gen <your_pkg>`
+
 
---
+++ app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/alexDtorres/gen"
+
+// app gen <your_app>
+func main() {
+	pkg := gen.Install()
+	Apply := pkg.Apply
+	Eval := pkg.Eval
+	pre := pkg.Pre
+	sess := pkg.Sess
+
+	pre.Check()
+
+	name := Eval(sess)
+	if err := Apply(name, sess); err != nil {
+		sess.Fatal(err)
+	}
+}
+
+// initial = `ls`
+// final   = concatenate(initial, `ls | grep ${name}`)
